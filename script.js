20da550bde75e7c78000cfb7fbce5bd9d22c6226
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2024-07-31 21:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,30 @@
 //your JS code here. If required.
 
 let codes = document.querySelectorAll(".code")
-
 // change, input
-
 codes.forEach((code,index)=>{
-
     
-
     code.addEventListener("input",()=>{
-
         if(code.value.length == 1){
-
              if(index < codes.length-1){
-
                    codes[index+1].focus()
-
+                   codes[index+1].classList.add("focused")
              }
-
         }
-
+    })
+    // keyup, keydown(press)
+    code.addEventListener("keydown",(e)=>{
+        if(e.key == "Backspace"){
+              if(index > 0 && code.value.length == 0){
+                
+                codes[index-1].focus()
+                codes[index-1].classList.add("focused")
+              }
+        }
     })
 
-    // keyup, keydown(press)
-
-    code.addEventListener("keydown",(e)=>{
-
-           console.log(e.key)
-
-        if(e.key == "Backspace"){
-
-              if(index > 0 && code.value.length == 0){
-
-                codes[index-1].focus()
-
-              }
-
-        }
-
+    code.addEventListener("blur", ()=>{
+          code.classList.remove("focused")
     })
 
 })
-
